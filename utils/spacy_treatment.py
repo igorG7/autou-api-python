158bb3ae21c4fcf1cbc20f4dd5224992a96b4805
igorG7 <igor.giovane.costa@gmail.com>
2025-10-01 01:51:25 -0300

    Refatoração da função spacyTreatment para apenas lematizar o conteúdo fornecido

--- utils/spacy_treatment.py
+++ utils/spacy_treatment.py
@@ -1,7 +1,10 @@
-
 import spacy
+from spacy.lang.pt.stop_words import STOP_WORDS
+
 nlp = spacy.load("pt_core_news_sm")
+keywords = {"sistema", "suporte"}
 
 def spacyTreatment(text):
+
     doc = nlp(text)
-    return [token.lemma_.lower() for token in doc if not token.is_stop and token.is_alpha]
+    return [token.lemma_ for token in doc if token.is_alpha]
