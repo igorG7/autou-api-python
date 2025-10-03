966d559339f56a0de613b8c5632ce1e786970327
igorG7 <igor.giovane.costa@gmail.com>
2025-10-02 21:17:38 -0300

    Captura de error e exceções na rota

--- Routes/file_routes.py
+++ Routes/file_routes.py
@@ -1,48 +1,57 @@
-from fastapi import APIRouter, UploadFile, File, Form
-from PyPDF2 import PdfReader
+from fastapi import APIRouter ,File, Form, UploadFile, HTTPException
 from typing import Optional
 import io
+from PyPDF2 import PdfReader
 
-from utils.send_content_agent import send_content_agent
+from services.llm_model import get_agent_response
+from utils.spacy_treatment import spacyTreatment
 
 file_router = APIRouter(prefix="/file", tags=["file"])
 
 @file_router.post("/")
-
 async def postFile(
     file: Optional[UploadFile] = File(None), 
     sender: Optional[str] = Form(None),  
     subject: Optional[str] = Form(None), 
     description: Optional[str] = Form(None)
-    ): 
+): 
 
     if sender and subject and description:
         text_form = f"Remetente: {sender} \n Assunto: {subject} \n Conteúdo: {description}"
-        response = await send_content_agent(text_form)
-        print(response)
-        return { "source": "form", "text": text_form }
- 
-    contents = await file.read()
+        agent_response = get_agent_response(spacyTreatment(text_form), text_form)
+        print(agent_response)
+        return agent_response
+
+  
+    if not file:
+        raise HTTPException(status_code=400, detail="Nenhuma informação arquivo foi enviado.")
+
+    try:
+        contents = await file.read()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao ler o arquivo: {e}")
 
     if file.content_type == "application/pdf":
-        pdf_file = io.BytesIO(contents)
-
-        reader =  PdfReader(pdf_file)
-        page = reader.pages[0]
-        text = page.extract_text()
-
-        response = await send_content_agent(text)
-        print(response)
-
-        return { "source": "pdf", "filename": file.filename, "text": text }
+        try:
+            pdf_file = io.BytesIO(contents)
+            reader = PdfReader(pdf_file)
+            page = reader.pages[0]
+            text = page.extract_text()
+            
+            print(agent_response)
+            return agent_response
+        except Exception:
+            raise HTTPException(status_code=400, detail="Erro ao processar o arquivo PDF. Pode estar corrompido.")
     
-    if file.content_type == "text/plain":
-        text = contents.decode("utf-8")
-        
-        response = await send_content_agent(text)
-        print(response)
-       
-        return { "source": "txt", "filename": file.filename, "text": text }
-        
+    elif file.content_type == "text/plain":
+        try:
+            text = contents.decode("utf-8")
+            agent_response = get_agent_response(spacyTreatment(text), text)
+            print(agent_response)
+            return agent_response
+        except UnicodeDecodeError:
+            raise HTTPException(status_code=400, detail="Erro ao decodificar arquivo de texto. Formato UTF-8 esperado.")
 
-    return { "message": "Formato inválido ou nenhum arquivo foi enviado." }
+    else:
+        raise HTTPException(status_code=400, detail="Formato de arquivo inválido. Apenas PDF ou TXT são aceitos.")
+
